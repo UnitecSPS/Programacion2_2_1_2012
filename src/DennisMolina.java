d719f86cce7ed403b25559bd8ce25d8361ef5f14
Dennis Noel Molina Garcia <dennismolina.17@gmail.com>
2012-07-20 09:41:38 -0600

    Crear clase

---
+++ src/DennisMolina.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sheila Garcia
+ */
+public class DennisMolina {
+   public static void main(String arg[]){
+   System.out.println("Dennis Molina");
+   
+   }
+}
